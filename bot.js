59d566924cb9983c7af329b9351c51546e3e7ae5
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-27 06:36:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,17 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const yumz = member.guild.channels.find("name", "invite-checker");
+     yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`);
+   //  yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
+
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
